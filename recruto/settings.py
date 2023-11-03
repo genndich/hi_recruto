a21a4bee32251096e26225107b27117d72d1a617
Pavel Baranov <ppvvlb@MacBook-Air-Pavel.local>
2023-11-03 19:05:42 +0300

    Первый коммит

--- recruto/settings.py
+++ recruto/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-EN'
 
-TIME_ZONE = 'UTC+3'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
